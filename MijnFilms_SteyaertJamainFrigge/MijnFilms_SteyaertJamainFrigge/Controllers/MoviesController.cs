1403465678075e2549ad3d337570ed97bd7e8ee1
Frigge Steyaert Jamain <frigge.steyaert.jamain@student.howest.be>
2018-04-23 11:43:52 +0200

    iets geprobeerd

--- MijnFilms_SteyaertJamainFrigge/MijnFilms_SteyaertJamainFrigge/Controllers/MoviesController.cs
+++ MijnFilms_SteyaertJamainFrigge/MijnFilms_SteyaertJamainFrigge/Controllers/MoviesController.cs
@@ -4,21 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MijnFilms_SteyaertJamainFrigge.Entities;
 
 namespace MijnFilms_SteyaertJamainFrigge.Controllers
 {
     public class MoviesController : Controller
     {
+        private List<Movie> Movies { get; set; }
+        public MoviesController()
+        {
+            
+        }
         // GET: Movies/List
         public IActionResult List()
         {
-            return View();
+            return View(Movies);
         }
 
         // GET: Movies/Details/
         public IActionResult Details(int id)
         {
-            return View();
+            return View(GetDetailsFromMovieID(id));
+        }
+
+        private Movie GetDetailsFromMovieID(int id)
+        {
+            //make connection to db and get movie with specific id
+            return new Movie(1, "test", 2007, 2, 1, 5, "this is a description");
         }
 
         // GET: Movies/Find
