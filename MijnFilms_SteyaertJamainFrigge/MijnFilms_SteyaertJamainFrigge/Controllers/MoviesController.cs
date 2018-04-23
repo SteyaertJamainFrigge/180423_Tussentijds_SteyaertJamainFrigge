f1472830eb46aa9c96e40c64cd0628c147abb8df
Frigge Steyaert Jamain <frigge.steyaert.jamain@student.howest.be>
2018-04-23 10:02:15 +0200

    HomeController en View verwijderd MovieController en ListView gemaakt

---
+++ MijnFilms_SteyaertJamainFrigge/MijnFilms_SteyaertJamainFrigge/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MijnFilms_SteyaertJamainFrigge.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/List
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        // GET: Movies/Details/
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Movies/Find
+        public IActionResult Find(string titel)
+        {
+            return View();
+        }
+
+    }
+}
--- MijnFilms_SteyaertJamainFrigge/MijnFilms_SteyaertJamainFrigge/Startup.cs
+++ MijnFilms_SteyaertJamainFrigge/MijnFilms_SteyaertJamainFrigge/Startup.cs
@@ -38,13 +38,14 @@ namespace MijnFilms_SteyaertJamainFrigge
             }
 
             app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
